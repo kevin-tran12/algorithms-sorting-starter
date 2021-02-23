1ee6fc4fd87679f128f2b755a3e45248d5648a8e
Kevin Tran <kevin.trancb@gmail.com>
2021-02-23 13:19:43 -0500

    merge sort complete

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -2,10 +2,36 @@
 
 function merge(array1, array2) {
   // your code here
+  let merged = []
+  // let leftIndex = 0
+  // let rightIndex = 0
+
+  while(array1.length ||array2.length){
+    let ele1 = array1.length ? array1[0] : Infinity
+    let ele2 = array2.length ? array2[0] : Infinity
+
+    let next;
+
+    if(ele1<ele2){
+      next = array1.shift()
+    } else{
+      next = array2.shift()
+    }
+    merged.push(next)
+  }
+  return merged
 }
 
 function mergeSort(array) {
   // your code here
+  if(array.length <= 1){
+    return array
+  }
+  let midIdx = Math.floor(array.length/2)
+  let leftHalf = array.slice(0,midIdx)
+  let rightHalf = array.slice(midIdx)
+
+  return merge(mergeSort(leftHalf),mergeSort(rightHalf))
 }
 
 module.exports = {
